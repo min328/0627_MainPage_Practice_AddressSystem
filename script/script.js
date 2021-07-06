83d2d88a949ab0c7c3c7be97c3858477024fcc03
min328 <kmj20212021@gmail.com>
2021-07-06 11:59:10 +0900

    GNB menu completed

--- script/script.js
+++ script/script.js
@@ -1,1 +1,22 @@
 /* eslint-disable */
+
+$(function () {
+    /* mainLi mouseover시 subMenu 발생1 - 시작 */
+        $("li.mainLi").mouseover(function () {
+            $("ul.subMenu").css({
+                "visibility": "visible"
+            });
+            $("div#headerGnbBG").css({
+                "display": "block"
+            });
+        });
+
+        $("li.mainLi").mouseout(function () {
+                    $("ul.subMenu").css({
+                        "visibility": "hidden"
+                    });
+                    $("div#headerGnbBG").css({
+                        "display": "none"
+                    });    });
+    /* mainLi mouseover시 subMenu 발생1 - 끝 */
+});
